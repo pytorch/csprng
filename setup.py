10c35662e921fb72e4a66e49f229363f906b05b4
Pavel Belevich <pbelevich@fb.com>
2020-06-19 15:17:16 -0700

    Fix OpenMP flags
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from torch.utils import cpp_extension
 
 build_cuda = cpp_extension.CUDA_HOME != None
 
+CXX_FLAGS = ['-fopenmp']
+NVCC_FLAGS = ['--expt-extended-lambda', '-Xcompiler', '-fopenmp']
+
 if build_cuda:
     csprng_ext = cpp_extension.CUDAExtension(
         'torch_csprng', ['csprng.cu'],
         extra_compile_args={'cxx': [],
-                            'nvcc': ['-O2', '--expt-extended-lambda']}
+                            'nvcc': NVCC_FLAGS}
     )
 else:
     csprng_ext = cpp_extension.CppExtension(
-        'torch_csprng', ['csprng.cpp']
+        'torch_csprng', ['csprng.cpp'],
+        extra_compile_args={'cxx': CXX_FLAGS}
     )
 
 setup(name='pytorch_csprng',
