3c11c5170eeea2b7586ca26711d48278ebeafeeb
Pavel Belevich <pbelevich@fb.com>
2020-06-19 10:17:03 -0700

    Cleaning setup.py
    ghstack-source-id: e915363a241e3aca7fd80537acf772f4191ffc33
    Pull Request resolved: https://github.com/pytorch/csprng/pull/29

--- setup.py
+++ setup.py
@@ -1,28 +1,17 @@
-import sys
-from setuptools import setup, Extension
+from setuptools import setup
 from torch.utils import cpp_extension
-
-if sys.platform == 'win32':
-    vc_version = os.getenv('VCToolsVersion', '')
-    if vc_version.startswith('14.16.'):
-        CXX_FLAGS = ['/sdl']
-    else:
-        CXX_FLAGS = ['/sdl', '/permissive-']
-else:
-    CXX_FLAGS = ['-g']
 
 build_cuda = cpp_extension.CUDA_HOME != None
 
 if build_cuda:
     csprng_ext = cpp_extension.CUDAExtension(
         'torch_csprng', ['csprng.cu'],
-        extra_compile_args={'cxx': CXX_FLAGS,
-                            'nvcc': ['-O2', '--expt-extended-lambda', '-DAT_PARALLEL_OPENMP=1']}
+        extra_compile_args={'cxx': [],
+                            'nvcc': ['-O2', '--expt-extended-lambda']}
     )
 else:
     csprng_ext = cpp_extension.CppExtension(
-        'torch_csprng', ['csprng.cpp'],
-        extra_compile_args={'cxx': CXX_FLAGS + ['-DAT_PARALLEL_OPENMP=1']}
+        'torch_csprng', ['csprng.cpp']
     )
 
 setup(name='pytorch_csprng',
