dc38be76304bf238bdbf5601eb078458834af992
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@chronos574.prn2.facebook.com>
2020-08-09 04:30:45 -0700

    2020-08-09 nightly release (7109581e8441e8b35229e6157593bb509aa760b4)

--- setup.py
+++ setup.py
@@ -1,53 +1,24 @@
 import os
-from sys import platform
+import sys
 import subprocess
-from setuptools import setup
+from setuptools import setup, find_packages
+import distutils.command.clean
+import glob
+import shutil
+
 import torch
-from torch.utils import cpp_extension
-
-build_cuda = torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1'
-
-CXX_FLAGS = os.getenv('CXX_FLAGS', '')
-if CXX_FLAGS == '':
-    CXX_FLAGS = []
-else:
-    CXX_FLAGS = CXX_FLAGS.split(' ')
-if platform == 'linux':
-    CXX_FLAGS.append('-fopenmp')
-
-NVCC_FLAGS = os.getenv('NVCC_FLAGS', '')
-if NVCC_FLAGS == '':
-    NVCC_FLAGS = []
-else:
-    NVCC_FLAGS = NVCC_FLAGS.split(' ')
-
-for flag in ['--expt-extended-lambda', '-Xcompiler', '-fopenmp']:
-    if not flag in NVCC_FLAGS:
-        NVCC_FLAGS.append(flag)
-
-module_name = 'torchcsprng'
-
-this_dir = os.path.dirname(os.path.abspath(__file__))
-extensions_dir = os.path.join(this_dir, module_name, 'csrc')
-
-if build_cuda:
-    csprng_ext = cpp_extension.CUDAExtension(
-        module_name, [os.path.join(extensions_dir, 'csprng.cu')],
-        extra_compile_args={'cxx': [],
-                            'nvcc': NVCC_FLAGS}
-    )
-else:
-    csprng_ext = cpp_extension.CppExtension(
-        module_name, [os.path.join(extensions_dir, 'csprng.cpp')],
-        extra_compile_args={'cxx': CXX_FLAGS}
-    )
+from torch.utils.cpp_extension import BuildExtension, CppExtension, \
+    CUDAExtension, CUDA_HOME
 
 version = open('version.txt', 'r').read().strip()
 sha = 'Unknown'
 package_name = 'torchcsprng'
 
+cwd = os.path.dirname(os.path.abspath(__file__))
+
 try:
-    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=this_dir).decode('ascii').strip()
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode(
+        'ascii').strip()
 except Exception:
     pass
 
@@ -57,11 +28,18 @@ elif sha != 'Unknown':
     version += '+' + sha[:7]
 print("Building wheel {}-{}".format(package_name, version))
 
-# Doesn't work yet :(
-# version_path = os.path.join(this_dir, module_name, 'version.py')
-# with open(version_path, 'w') as f:
-#     f.write("__version__ = '{}'\n".format(version))
-#     f.write("git_version = {}\n".format(repr(sha)))
+
+def write_version_file():
+    version_path = os.path.join(cwd, 'torchcsprng', 'version.py')
+    with open(version_path, 'w') as f:
+        f.write("__version__ = '{}'\n".format(version))
+        f.write("git_version = {}\n".format(repr(sha)))
+        # f.write("from torchcsprng.extension import _check_cuda_version\n")
+        # f.write("if _check_cuda_version() > 0:\n")
+        # f.write("    cuda = _check_cuda_version()\n")
+
+
+write_version_file()
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -73,6 +51,79 @@ if os.getenv('PYTORCH_VERSION'):
 requirements = [
     pytorch_dep,
 ]
+
+
+def get_extensions():
+    build_cuda = torch.cuda.is_available() or os.getenv('FORCE_CUDA',
+                                                        '0') == '1'
+
+    module_name = 'torchcsprng'
+
+    extensions_dir = os.path.join(cwd, module_name, 'csrc')
+
+    openmp = 'ATen parallel backend: OpenMP' in torch.__config__.parallel_info()
+
+    if build_cuda:
+        sources = [os.path.join(extensions_dir, 'csprng.cu')]
+        extension = CUDAExtension
+        nvcc_flags = os.getenv('NVCC_FLAGS', '')
+        if nvcc_flags == '':
+            nvcc_flags = []
+        else:
+            nvcc_flags = nvcc_flags.split(' ')
+        for flag in ['--expt-extended-lambda', '-Xcompiler', '-fopenmp']:
+            if not flag in nvcc_flags:
+                nvcc_flags.append(flag)
+        extra_compile_args = {
+            'cxx': [],
+            'nvcc': nvcc_flags,
+        }
+    else:
+        sources = [os.path.join(extensions_dir, 'csprng.cpp')]
+        extension = CppExtension
+        cxx_flags = os.getenv('CXX_FLAGS', '')
+        if cxx_flags == '':
+            cxx_flags = []
+        else:
+            cxx_flags = cxx_flags.split(' ')
+        if sys.platform == 'linux' and openmp:
+            for flag in ['-fopenmp']:
+                if not flag in cxx_flags:
+                    cxx_flags.append(flag)
+        extra_compile_args = {
+            'cxx': cxx_flags
+        }
+
+    ext_modules = [
+        extension(
+            module_name + '._C',
+            sources,
+            extra_compile_args=extra_compile_args,
+        )
+    ]
+
+    return ext_modules
+
+
+class clean(distutils.command.clean.clean):
+    def run(self):
+        with open('.gitignore', 'r') as f:
+            ignores = f.read()
+            start_deleting = False
+            for wildcard in filter(None, ignores.split('\n')):
+                if wildcard == '# do not change or delete this comment - `python setup.py clean` deletes everything after this line':
+                    start_deleting = True
+                if not start_deleting:
+                    continue
+                for filename in glob.glob(wildcard):
+                    try:
+                        os.remove(filename)
+                    except OSError:
+                        shutil.rmtree(filename, ignore_errors=True)
+
+        # It's an old-style class in Python 2.7...
+        distutils.command.clean.clean.run(self)
+
 
 setup(
     # Metadata
@@ -87,6 +138,7 @@ setup(
     license='BSD-3',
 
     # Package info
+    packages=find_packages(exclude=('test',)),
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
@@ -106,6 +158,10 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=requirements,
-    ext_modules=[csprng_ext],
-    cmdclass={'build_ext': cpp_extension.BuildExtension}
+    ext_modules=get_extensions(),
+    test_suite='test',
+    cmdclass={
+        'build_ext': BuildExtension,
+        'clean': clean,
+    }
 )
--- torchcsprng/__init__.py
+++ torchcsprng/__init__.py
@@ -1,1 +1,4 @@
 import torch
+del torch
+
+from torchcsprng._C import *
