49506511c547cb3522c20ab90ed7cf8b4262bb61
Pavel Belevich <pbelevich@fb.com>
2020-12-04 09:37:47 -0500

    Update on "separate cpp and cuda compilation"
    [ghstack-poisoned]

--- test/test_csprng.py
+++ test/test_csprng.py
@@ -309,9 +309,9 @@ class TestCSPRNG(unittest.TestCase):
         time_for_1K = measure(1000)
         time_for_1M = measure(1000000)
         # Pessimistic check that parallel execution gives >= 1.5 performance boost
-        print('torch.get_num_threads() =', torch.get_num_threads())
-        print('time_for_1K =', time_for_1K)
-        print('time_for_1M =', time_for_1M)
+        # print('torch.get_num_threads() =', torch.get_num_threads())
+        # print('time_for_1K =', time_for_1K)
+        # print('time_for_1M =', time_for_1M)
         self.assertTrue(False)
         self.assertTrue(time_for_1M/time_for_1K < 1000 / 1.5)
 
