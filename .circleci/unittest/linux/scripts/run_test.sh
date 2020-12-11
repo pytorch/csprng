2823bd69c03ffd2639970f37dff56a145d55e667
Pavel Belevich <pbelevich@fb.com>
2020-12-11 10:00:27 -0500

    Update on "Python 3.9 support"
    [ghstack-poisoned]

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,5 +6,4 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
-#pytest --cov=torchcsprng --junitxml=test-results/junit.xml -v --durations 20 test
-pytest --junitxml=test-results/junit.xml -v --durations 20 test
+pytest --cov=torchcsprng --junitxml=test-results/junit.xml -v --durations 20 test
