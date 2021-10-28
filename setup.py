5a6d9458c142190d5d713744687434c73c06ad01
Tim Rijavec <timrijavec@fb.com>
2021-10-28 05:06:30 -0700

    Opt-in "fbcode/pytorch/(pLBFGS|pytorch-lbfgs|contrib|csprng|cutlass-2.5.0)" for BLACK auto formatting
    Summary:
    Diff 2/n in series of opting pytorch into BLACK auto formatting
    Opt-in
    - "fbcode/pytorch/pLBFGS/",
    - "fbcode/pytorch/contrib/",
    - "fbcode/pytorch/csprng/",
    
    Third-party filters
    - "fbcode/pytorch/pytorch-lbfgs/",
    - "fbcode/pytorch/cutlass-2.5.0/",
    
    for BLACK auto formatting https://fb.workplace.com/groups/pythonfoundation/posts/2990917737888352/
    Next steps: going through the rest of pytorch directories
    
    Reviewed By: zertosh
    
    Differential Revision: D31959671
    
    fbshipit-source-id: f50e9b88bebc729a1e118266a7301e90353660c7

--- setup.py
+++ setup.py
@@ -1,37 +1,44 @@
-import os
-import sys
-import subprocess
-from setuptools import setup, find_packages
 import distutils.command.clean
 import glob
+import os
 import shutil
+import subprocess
+import sys
 
 import torch
-from torch.utils.cpp_extension import BuildExtension, CppExtension, \
-    CUDAExtension, CUDA_HOME
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import (
+    BuildExtension,
+    CppExtension,
+    CUDAExtension,
+    CUDA_HOME,
+)
 
-version = open('version.txt', 'r').read().strip()
-sha = 'Unknown'
-package_name = 'torchcsprng'
+version = open("version.txt", "r").read().strip()
+sha = "Unknown"
+package_name = "torchcsprng"
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 try:
-    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode(
-        'ascii').strip()
+    sha = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=cwd)
+        .decode("ascii")
+        .strip()
+    )
 except Exception:
     pass
 
-if os.getenv('BUILD_VERSION'):
-    version = os.getenv('BUILD_VERSION')
-elif sha != 'Unknown':
-    version += '+' + sha[:7]
+if os.getenv("BUILD_VERSION"):
+    version = os.getenv("BUILD_VERSION")
+elif sha != "Unknown":
+    version += "+" + sha[:7]
 print("Building wheel {}-{}".format(package_name, version))
 
 
 def write_version_file():
-    version_path = os.path.join(cwd, 'torchcsprng', 'version.py')
-    with open(version_path, 'w') as f:
+    version_path = os.path.join(cwd, "torchcsprng", "version.py")
+    with open(version_path, "w") as f:
         f.write("__version__ = '{}'\n".format(version))
         f.write("git_version = {}\n".format(repr(sha)))
         # f.write("from torchcsprng.extension import _check_cuda_version\n")
@@ -44,9 +51,9 @@ write_version_file()
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-pytorch_dep = 'torch'
-if os.getenv('PYTORCH_VERSION'):
-    pytorch_dep += "==" + os.getenv('PYTORCH_VERSION')
+pytorch_dep = "torch"
+if os.getenv("PYTORCH_VERSION"):
+    pytorch_dep += "==" + os.getenv("PYTORCH_VERSION")
 
 requirements = [
     pytorch_dep,
@@ -61,61 +68,60 @@ def append_flags(flags, flags_to_append):
 
 
 def get_extensions():
-    build_cuda = torch.cuda.is_available() or os.getenv('FORCE_CUDA',
-                                                        '0') == '1'
+    build_cuda = torch.cuda.is_available() or os.getenv("FORCE_CUDA", "0") == "1"
 
-    module_name = 'torchcsprng'
+    module_name = "torchcsprng"
 
-    extensions_dir = os.path.join(cwd, module_name, 'csrc')
+    extensions_dir = os.path.join(cwd, module_name, "csrc")
 
-    openmp = 'ATen parallel backend: OpenMP' in torch.__config__.parallel_info()
+    openmp = "ATen parallel backend: OpenMP" in torch.__config__.parallel_info()
 
-    main_file = glob.glob(os.path.join(extensions_dir, '*.cpp'))
-    source_cpu = glob.glob(os.path.join(extensions_dir, 'cpu', '*.cpp'))
+    main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
+    source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
 
     sources = main_file + source_cpu
     extension = CppExtension
 
     define_macros = []
 
-    cxx_flags = os.getenv('CXX_FLAGS', '')
-    if cxx_flags == '':
+    cxx_flags = os.getenv("CXX_FLAGS", "")
+    if cxx_flags == "":
         cxx_flags = []
     else:
-        cxx_flags = cxx_flags.split(' ')
+        cxx_flags = cxx_flags.split(" ")
     if openmp:
-        if sys.platform == 'linux':
-            cxx_flags = append_flags(cxx_flags, ['-fopenmp'])
-        elif sys.platform == 'win32':
-            cxx_flags = append_flags(cxx_flags, ['/openmp'])
+        if sys.platform == "linux":
+            cxx_flags = append_flags(cxx_flags, ["-fopenmp"])
+        elif sys.platform == "win32":
+            cxx_flags = append_flags(cxx_flags, ["/openmp"])
         # elif sys.platform == 'darwin':
         #     cxx_flags = append_flags(cxx_flags, ['-Xpreprocessor', '-fopenmp'])
 
     if build_cuda:
         extension = CUDAExtension
-        source_cuda = glob.glob(os.path.join(extensions_dir, 'cuda', '*.cu'))
+        source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
         sources += source_cuda
 
-        define_macros += [('WITH_CUDA', None)]
+        define_macros += [("WITH_CUDA", None)]
 
-        nvcc_flags = os.getenv('NVCC_FLAGS', '')
-        if nvcc_flags == '':
+        nvcc_flags = os.getenv("NVCC_FLAGS", "")
+        if nvcc_flags == "":
             nvcc_flags = []
         else:
-            nvcc_flags = nvcc_flags.split(' ')
-        nvcc_flags = append_flags(nvcc_flags, ['--expt-extended-lambda', '-Xcompiler'])
+            nvcc_flags = nvcc_flags.split(" ")
+        nvcc_flags = append_flags(nvcc_flags, ["--expt-extended-lambda", "-Xcompiler"])
         extra_compile_args = {
-            'cxx': cxx_flags,
-            'nvcc': nvcc_flags,
+            "cxx": cxx_flags,
+            "nvcc": nvcc_flags,
         }
     else:
         extra_compile_args = {
-            'cxx': cxx_flags,
+            "cxx": cxx_flags,
         }
 
     ext_modules = [
         extension(
-            module_name + '._C',
+            module_name + "._C",
             sources,
             define_macros=define_macros,
             extra_compile_args=extra_compile_args,
@@ -127,11 +133,14 @@ def get_extensions():
 
 class clean(distutils.command.clean.clean):
     def run(self):
-        with open('.gitignore', 'r') as f:
+        with open(".gitignore", "r") as f:
             ignores = f.read()
             start_deleting = False
-            for wildcard in filter(None, ignores.split('\n')):
-                if wildcard == '# do not change or delete this comment - `python setup.py clean` deletes everything after this line':
+            for wildcard in filter(None, ignores.split("\n")):
+                if (
+                    wildcard
+                    == "# do not change or delete this comment - `python setup.py clean` deletes everything after this line"
+                ):
                     start_deleting = True
                 if not start_deleting:
                     continue
@@ -155,34 +164,33 @@ setup(
     description="Cryptographically secure pseudorandom number generators for PyTorch",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='BSD-3',
-
+    license="BSD-3",
     # Package info
-    packages=find_packages(exclude=('test',)),
+    packages=find_packages(exclude=("test",)),
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements,
     ext_modules=get_extensions(),
-    test_suite='test',
+    test_suite="test",
     cmdclass={
-        'build_ext': BuildExtension,
-        'clean': clean,
-    }
+        "build_ext": BuildExtension,
+        "clean": clean,
+    },
 )
