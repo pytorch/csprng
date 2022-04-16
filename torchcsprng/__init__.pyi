447321e266a86a25655a092d79e4a8f8c15d6b38
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared28090.06.pnb2.facebook.com>
2022-04-16 04:30:28 -0700

    2022-04-16 nightly release (c34940e2415c39e67b720d71d7ab678be3bbe18f)

--- torchcsprng/__init__.pyi
+++ torchcsprng/__init__.pyi
@@ -3,7 +3,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torch import Tensor, Generator
+from torch import Generator, Tensor
 
 def supports_cuda() -> bool: ...
 def create_random_device_generator(token: str = "") -> Generator: ...
