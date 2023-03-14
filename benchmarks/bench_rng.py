e71478ab72fbaf2fc4ef08dae1f6c279438ce92c
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2023-03-14 20:28:13 +0000

    Add profiling to the CSPRNG benchmarking script.

--- benchmarks/bench_rng.py
+++ benchmarks/bench_rng.py
@@ -4,6 +4,9 @@ import argparse
 import torch
 import torchcsprng as csprng
 import time
+from contextlib import ExitStack
+from pathlib import Path
+from torch.profiler import profile, record_function, tensorboard_trace_handler, ProfilerActivity
 from typing import Tuple
 
 
@@ -13,20 +16,45 @@ def bench_randint(
     device: torch.device = torch.device("cpu"),
     n_trials: int = 10,
 ) -> float:
-    # Warmup
-    args = (-2**63, 2**63-1, size)
-    kwargs = {
-        "dtype": torch.int64,
-        "device": device,
-        "generator": generator
-    }
-    _ = torch.randint(*args, **kwargs)
+    with record_function("bench_randint"):
+        # Warmup
+        args = (-2**63, 2**63-1, size)
+        kwargs = {
+            "dtype": torch.int64,
+            "device": device,
+            "generator": generator
+        }
+        for _ in range(5):
+            x = torch.randint(*args, **kwargs)
 
-    t0 = time.time()
-    for _ in range(n_trials):
-        x = torch.randint(*args, **kwargs)
-    t1 = time.time()
-    return t1 - t0
+        t0 = time.time()
+        for _ in range(n_trials):
+            x = torch.randint(*args, **kwargs)
+        t1 = time.time()
+        return t1 - t0
+
+def bench_randn(
+    size: Tuple[int, ...],
+    generator: torch.Generator,
+    device: torch.device = torch.device("cpu"),
+    n_trials: int = 10,
+) -> float:
+    with record_function("bench_randn"):
+        # Warmup
+        args = (size,)
+        kwargs = {
+            "dtype": torch.float32,
+            "device": device,
+            "generator": generator
+        }
+        for _ in range(5):
+            x = torch.randn(*args, **kwargs)
+
+        t0 = time.time()
+        for _ in range(n_trials):
+            x = torch.randn(*args, **kwargs)
+        t1 = time.time()
+        return t1 - t0
 
 
 if __name__ == "__main__":
@@ -47,14 +75,36 @@ if __name__ == "__main__":
         default=2**10,
         help="Number of random elements to generate",
     )
+    parser.add_argument(
+        "--profile",
+        action="store_true",
+        help="Run PyTorch profiler"
+    )
 
     args = parser.parse_args()
     device = torch.device(args.device)
 
-    generator = torch.Generator()
-    time_default = bench_randint((args.size,), generator, device)
-    print(f"Default RNG time: {time_default:.4f}")
+    with ExitStack() as stack:
+        if args.profile:
+            tracedir = Path.cwd() / "traces"
+            tracedir.mkdir(exist_ok=True)
+            print("Removing old profiling traces")
+            for old_trace in tracedir.glob("*.json.gz"):
+                old_trace.unlink()
+                print(f"Removed {str(old_trace)}")
 
-    generator = csprng.create_random_device_generator()
-    time_csprng = bench_randint((args.size,), generator, device)
-    print(f"CSPRNG time: {time_csprng:.4f}")
+            trace_handler = tensorboard_trace_handler(tracedir, use_gzip=True)
+            kwargs = {
+                "record_shapes": True,
+                "with_stack": True,
+                "on_trace_ready": trace_handler,
+            }
+            stack.enter_context(profile(activities=[ProfilerActivity.CPU], **kwargs))
+
+        gen = torch.Generator()
+        print(f"Default RNG time:   {bench_randint((args.size,), gen, device):.4f}")
+        print(f"Default randn time: {bench_randn((args.size,), gen, device):.4f}")
+
+        gen = csprng.create_stream_generator()
+        print(f"CSPRNG time:        {bench_randint((args.size,), gen, device):.4f}")
+        print(f"CSPRNG time:        {bench_randn((args.size,), gen, device):.4f}")
